0ea584207fdd22e3ed127fe086398f7dfa5c7e18
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-03-22 10:02:03 +0800

    added color styles

---
+++ src/styles/colors.js
@@ -0,0 +1,29 @@
+export default {
+  white: '#FFF',
+  black: '#000',
+  primary: '#F78F1E',
+  secondary: '#0D2943',
+  accent: '',
+  disabled: '#C5CBD2',
+  danger: '#C10E0E',
+  warning: '#A87E6F',
+
+  ongoing: '#F5A623',
+  won: '#417505',
+  inspected: '#4A90E2',
+
+  googleRed: '#DE4D3B',
+  facebookBlue: '#4267B2',
+
+  lightGrey: '#E5E5E5',
+  midGrey: '#676870',
+  grey: '#D0D0D0',
+
+  text: {
+    black: '#272833',
+  },
+  label: {
+    grey: '#929398',
+  },
+
+};
