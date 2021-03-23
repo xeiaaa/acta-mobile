58071971c236796479f724fb38ef2b73fcc8f6af
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-03-23 21:50:00 +0800

    add button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+// Components
+import Text from '../Text/Text';
+
+import { button, colors } from '../../styles';
+
+const Button = ({
+  buttonStyle, textStyle, text, type, touchableProps, outline,
+}) => {
+  let defaultStyle = { ...button.button };
+
+  if (outline) {
+    defaultStyle = { ...defaultStyle, ...button.outline, borderColor: colors[type] };
+  } else {
+    defaultStyle = { ...defaultStyle, ...button[type] };
+  }
+
+  return (
+    <TouchableOpacity
+      style={{
+        ...defaultStyle,
+        ...buttonStyle,
+      }}
+      activeOpacity={0.7}
+      {...touchableProps}
+    >
+      <Text style={{
+        color: outline ? colors[type] : type === 'white' ? colors.text.black : colors.white,
+        ...textStyle,
+      }}
+      >
+        { text.toUpperCase() }
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+Button.defaultProps = {
+  buttonStyle: {},
+  textStyle: {},
+  text: '',
+  type: 'primary',
+  touchableProps: {},
+  outline: false,
+};
+
+Button.propTypes = {
+  buttonStyle: PropTypes.object,
+  textStyle: PropTypes.object,
+  text: PropTypes.string,
+  type: PropTypes.oneOf(['white', 'primary', 'secondary', 'accent', 'disabled', 'danger', 'warning', 'fb', 'google']),
+  touchableProps: PropTypes.object,
+  outline: PropTypes.bool,
+};
+
+export default Button;
