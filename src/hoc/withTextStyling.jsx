fe7dcdfef32688ab5304c9fb2dfce3776899a7d3
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-03-26 13:39:29 +0800

    add hoc withTextStyling

---
+++ src/hoc/withTextStyling.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { marginParser, paddingParser } from '../lib/helpers';
+
+const withTextStyling = (Component) => {
+  const EnhancedComponent = (props) => {
+    const newProps = { ...props };
+    const {
+      margin, padding, style,
+    } = newProps;
+
+    const styles = {
+      ...marginParser(margin),
+      ...paddingParser(padding),
+      ...style,
+    };
+    if (margin) {
+      console.log({ styles });
+    }
+
+    newProps.style = styles;
+    newProps.margin = undefined;
+    newProps.padding = undefined;
+
+    return (
+      <Component {...newProps} />
+    );
+  };
+
+  EnhancedComponent.propTypes = {
+    margin: PropTypes.string,
+    padding: PropTypes.string,
+    style: PropTypes.object,
+  };
+
+  EnhancedComponent.defaultProps = {
+    margin: '',
+    padding: '',
+    style: {},
+  };
+
+  return EnhancedComponent;
+};
+
+export default withTextStyling;
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,1 +1,74 @@
+import { sizing } from '../styles';
+
 export const phpFormat = (num) => num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+export const marginParser = (str = '') => {
+  /*
+    str = 'b.12-t.sm-l.xl'
+    margin = {
+      botttom: 12,
+      top: sizing.margin.sm,
+      left: sizing.margin.l
+    }
+  */
+  const mapping = {
+    b: 'marginBottom',
+    t: 'marginTop',
+    l: 'marginLeft',
+    r: 'marginRight',
+    h: 'marginHorizontal',
+    v: 'marginVertical',
+  };
+
+  let margin = {};
+
+  const styles = str.split('-');
+  styles.forEach((s) => {
+    const [direction, value] = s.split('.');
+
+    // validate direction and value
+    if (mapping[direction]) {
+      // check if value = int or enum[xxs, xs, s, m, l, xl, xxl]
+      const intValue = parseInt(value, 10);
+      if (Number.isInteger(intValue)) {
+        margin = { ...margin, [mapping[direction]]: intValue };
+      } else {
+        margin = { ...margin, [mapping[direction]]: sizing.margin[value] };
+      }
+    }
+  });
+
+  return margin;
+};
+
+export const paddingParser = (str = '') => {
+  const mapping = {
+    b: 'paddingBottom',
+    t: 'paddingTop',
+    l: 'paddingLeft',
+    r: 'paddingRight',
+    h: 'paddingHorizontal',
+    v: 'paddingVertical',
+  };
+
+  let padding = {};
+
+  const styles = str.split('-');
+
+  styles.forEach((s) => {
+    const [direction, value] = s.split('.');
+
+    // validate direction and value
+    if (mapping[direction]) {
+      // check if value = int or enum[xxs, xs, s, m, l, xl, xxl]
+      const intValue = parseInt(value, 10);
+      if (Number.isInteger(intValue)) {
+        padding = { ...padding, [mapping[direction]]: intValue };
+      } else {
+        padding = { ...padding, [mapping[direction]]: sizing.padding[value] };
+      }
+    }
+  });
+
+  return padding;
+};
