be27885870d3a83eab054a9dcca73791f8b257fd
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-03-27 17:10:47 +0800

    update screen home

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import {
-  ScrollView, StyleSheet, View, TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 // Components
 import ProductHorizontalList from '../components/ProductHorizontalList/ProductHorizontalList';
 import CustomHeaderLayout from '../components/Layouts/CustomHeaderLayout';
-import { sizing, typography } from '../styles';
 
 // mock data
 import { products } from '../lib/mockData';
-import Text from '../components/Text/Text';
+import ListTitle from '../components/ListTitle/ListTitle';
 
 const HomeScreen = ({ navigation }) => (
   <CustomHeaderLayout title="Feed">
     <ScrollView>
-      <View style={styles.header}>
-        <Text style={typography.listTitle}>Featured Products</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('FeaturedProductsScreen')}>
-          <Text type="light-s-primary">
-            See All
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <ListTitle
+        action={() => navigation.navigate('FeaturedProductsScreen')}
+        actionText="See All"
+        title="Featured Products"
+      />
       <ProductHorizontalList
         products={products}
         title="Featured Products"
         seeAll={() => navigation.navigate('FeaturedProductsScreen')}
         navigation={navigation}
       />
+
+      <ListTitle
+        action={() => navigation.navigate('FeaturedProductsScreen')}
+        actionText="See All"
+        title="Latest Products"
+        margin="t.xl"
+      />
       <ProductHorizontalList
         products={products}
         title="Latest Products"
-        containerStyle={{
-          marginTop: sizing.margin.xl,
-        }}
         navigation={navigation}
       />
     </ScrollView>
@@ -49,14 +49,5 @@ HomeScreen.defaultProps = {
 HomeScreen.propTypes = {
   navigation: PropTypes.any,
 };
-
-const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: sizing.padding.m,
-  },
-});
 
 export default HomeScreen;
