6f6a14d6845f795c0b115a21acbbee7b1b60a2a3
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-03-31 11:38:02 +0800

    update screen Signin

--- src/screens/SigninScreen.jsx
+++ src/screens/SigninScreen.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
-  View, TouchableOpacity,
+  View as RNView, TouchableOpacity, Alert, ScrollView, TouchableWithoutFeedback, Keyboard,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -10,91 +10,127 @@ import Button from '../components/Button/Button';
 import FullTitle from '../components/FullTitle/FullTitle';
 import LabelInput from '../components/Form/LabelInput';
 import CustomHeaderLayout from '../components/Layouts/CustomHeaderLayout';
+import withTextStyling from '../hoc/withTextStyling';
 
-import { colors, sizing } from '../styles';
+import { globals } from '../styles';
+import { delay } from '../lib/helpers';
+import { login } from '../lib/api';
+
+const View = withTextStyling(RNView);
 
 const SigninScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(null);
+  const passwordRef = useRef(null);
+
+  const onSubmit = async () => {
+    if (!email || !password || isLoading) {
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      const { data } = await login(email, password);
+
+      await delay(500);
+      setIsLoading(false);
+      Alert.alert('Welcome!', 'You are successfully logged in!');
+      console.log({ data });
+    } catch (err) {
+      setIsLoading(false);
+      Alert.alert('Error!', err.response.data.error || 'An unknown error occurred!');
+      console.log(err.response);
+    }
+  };
+
   return (
     <CustomHeaderLayout title="LOG IN">
-      <View>
-        <FullTitle title="Let's sign you in" subtitle="Welcome back, you’ve been missed!" />
-        <LabelInput
-          labelText="Username or Email"
-          textInputProps={{
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView>
+          <FullTitle title="Let's sign you in" subtitle="Welcome back, you’ve been missed!" />
+          <LabelInput
+            labelText="Email Address"
+            textInputProps={{
             // value: 'deemid',
-          }}
-        />
-        <LabelInput
-          labelText="Password"
-          textInputProps={{
-            secureTextEntry: !isPasswordVisible,
-          }}
-          hasIconButton
-          iconName={!isPasswordVisible ? 'eye' : 'eye-off'}
-          iconOnPress={() => setIsPasswordVisible(!isPasswordVisible)}
-          containerStyle={{ marginTop: 19 }}
-        />
-
-        <View style={{ paddingHorizontal: 30, marginTop: 32, alignItems: 'flex-end' }}>
-          <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('ForgotPasswordScreen')}>
-            <Text style={{
-              color: colors.text.black,
-              fontSize: sizing.font.s,
+              autoCapitalize: 'none',
+              keyboardType: 'email-address',
+              returnKeyType: 'next',
+              onSubmitEditing: () => {
+                passwordRef.current.focus();
+              },
+              onChangeText: setEmail,
+              value: email,
+              placeholder: 'juandelacruz@gmail.com',
             }}
-            >
-              Forgot Password?
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={{ alignItems: 'center', marginTop: 40 }}>
-          <Button
-            text="sign in"
-            type="secondary"
           />
-          <Button
-            text="continue with google"
-            type="google"
-            buttonStyle={{
-              marginTop: 16,
+          <LabelInput
+            labelText="Password"
+            textInputProps={{
+              secureTextEntry: !isPasswordVisible,
+              ref: passwordRef,
+              returnKeyType: 'go',
+              onSubmitEditing: onSubmit,
+              onChangeText: setPassword,
+              value: password,
+              placeholder: '********',
             }}
-            outline
+            hasIconButton
+            iconName={!isPasswordVisible ? 'eye' : 'eye-off'}
+            iconOnPress={() => setIsPasswordVisible(!isPasswordVisible)}
+            containerStyle={{ marginTop: 19 }}
           />
-          <Button
-            text="continue with fb"
-            type="fb"
-            buttonStyle={{
-              marginTop: 16,
-            }}
-            outline
-          />
-          <View style={{ flexDirection: 'row', marginTop: 16 }}>
-            <Text style={{
-              fontSize: sizing.font.s,
-              fontFamily: 'AvenirBook',
-              color: colors.midGrey,
-            }}
-            >
-              Don&#39;t have an account?
-              {' '}
 
-            </Text>
-            <TouchableOpacity>
-              <Text style={{
-                fontSize: sizing.font.s,
-                fontFamily: 'AvenirBook',
-                color: colors.text.black,
-              }}
-              >
-                Sign Up
-
+          <View style={{ paddingHorizontal: 30, marginTop: 32, alignItems: 'flex-end' }}>
+            <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('ForgotPasswordScreen')}>
+              <Text type="medium-s-textBlack">
+                Forgot Password?
               </Text>
             </TouchableOpacity>
           </View>
-        </View>
-      </View>
 
+          <View style={{ alignItems: 'center', marginTop: 40 }}>
+            <Button
+              text="sign in"
+              type={!email || !password ? 'disabled' : 'secondary'}
+              isLoading={isLoading}
+              touchableProps={{
+                disabled: (!email || !password) || isLoading,
+                onPress: onSubmit,
+              }}
+            />
+            <Button
+              text="continue with google"
+              type="google"
+              buttonStyle={{
+                marginTop: 16,
+              }}
+              outline
+            />
+            <Button
+              text="continue with fb"
+              type="fb"
+              buttonStyle={{
+                marginTop: 16,
+              }}
+              outline
+            />
+            <View style={{ ...globals.flexRow }} margin="t.m">
+              <Text type="light-s-midGrey" margin="r.xxs">
+                Don&#39;t have an account?
+              </Text>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => navigation.navigate('SignupScreen')}
+              >
+                <Text type="light-s-textBlack">Sign Up</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+
+      </TouchableWithoutFeedback>
     </CustomHeaderLayout>
 
   );
